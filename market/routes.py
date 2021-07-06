4dd3fb3bf4238aad138edc3e5dbeb02d53060cec
Your Name <you@example.com>
2021-07-07 00:07:51 +0200

    Flashes & Advanced Validations, User Authentication Part 1

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 @app.route("/")
@@ -18,11 +18,18 @@ def market_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password1.data)
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f'There was an error with creating an user: {err_msg}')
+            flash(f'There was an error with creating an user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
